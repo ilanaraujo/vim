14022e92ce1081295072813195946afadd5c07e7
Ilan Araujo <ilan.araujo@labnet.nce.ufrj.br>
2021-06-06 16:00:42 -0300

    Modificações no script de configuração

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
-mv vimrc ../
-mv viminfo ../
-mv viminfo .viminfo
-mv vimrc .vimrc
+git clone https://github.com/ilanaraujo/vim
+cd vim
+mv .vimrc ../
+mv .viminfo ../
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+vim -c ":PluginInstall"
